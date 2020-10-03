02f70f25b94f6ab9271d63a32b1322a3ed3b8b35
Vlasta Vetrae <vlastavetra@gmail.com>
2020-10-03 23:25:53 +0300

    Удалены лишние пробелы в строках с фамилией

--- js/setup.js
+++ js/setup.js
@@ -7,7 +7,7 @@ const SIMILAR_WIZARD_TEMPLATE = document.querySelector(`#similar-wizard-template
     .content
     .querySelector(`.setup-similar-item`);
 const WIZARD_FIRST_NAME = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARD_LAST_NAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, ` Онопко`, ` Топольницкая`, ` Нионго`, ` Ирвинг`];
+const WIZARD_LAST_NAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const WIZARD_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 const FRAGMENT = document.createDocumentFragment();
