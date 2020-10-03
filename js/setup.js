94bc0c3fe1daa4d917d642d93483c3c11a5d24a3
Vlasta Vetrae <vlastavetra@gmail.com>
2020-10-03 23:22:59 +0300

    Правки по заданию Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,35 +1,43 @@
 'use strict';
 
-const USER_DIALOG = document.querySelector(`.setup`);
-const USER_SIMILAR = document.querySelector(`.setup-similar`);
+const USER_DIALOG_CN = `.setup`;
+const USER_SIMILAR_CN = `.setup-similar`;
 const SIMILAR_LIST_ELEMENT = document.querySelector(`.setup-similar-list`);
 const SIMILAR_WIZARD_TEMPLATE = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
 const WIZARD_FIRST_NAME = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARD_LAST_NAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const WIZARD_LAST_NAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, ` Онопко`, ` Топольницкая`, ` Нионго`, ` Ирвинг`];
 const WIZARD_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 const FRAGMENT = document.createDocumentFragment();
+const WIZARDS_NUMBER = 4;
 
-USER_DIALOG.classList.remove(`hidden`);
-
-let wizardsList = [];
-
-let createRandomElement = (arr) => {
+let getRandomElement = (arr) => {
   return arr[Math.round(Math.random() * Math.round(arr.length - 1))];
 };
 
-let createWisardsList = (number) => {
-  for (let i = 0; i < number; i++) {
-    wizardsList[i] = {};
-    wizardsList[i].name = createRandomElement(WIZARD_FIRST_NAME) + ` ` + createRandomElement(WIZARD_LAST_NAME);
-    wizardsList[i].coatColor = createRandomElement(WIZARD_COAT_COLOR);
-    wizardsList[i].eyesColor = createRandomElement(WIZARD_EYES_COLOR);
-  }
+let removeClass = (element, className) => {
+  element.querySelector(className).classList.remove(`hidden`);
 };
 
-createWisardsList(4);
+removeClass(document, USER_DIALOG_CN);
+removeClass(document, USER_SIMILAR_CN);
+
+let renderWisardsList = (number) => {
+  let wizard = [];
+
+  for (let i = 0; i < number; i++) {
+    wizard[i] = {};
+    wizard[i].name = getRandomElement(WIZARD_FIRST_NAME) + ` ` + getRandomElement(WIZARD_LAST_NAME);
+    wizard[i].coatColor = getRandomElement(WIZARD_COAT_COLOR);
+    wizard[i].eyesColor = getRandomElement(WIZARD_EYES_COLOR);
+  }
+
+  return wizard;
+};
+
+let wizardsList = renderWisardsList(WIZARDS_NUMBER);
 
 let renderWizard = (wizard) => {
   const WIZARD_ELEMENT = SIMILAR_WIZARD_TEMPLATE.cloneNode(true);
@@ -46,5 +54,3 @@ for (let i = 0; i < wizardsList.length; i++) {
 }
 
 SIMILAR_LIST_ELEMENT.appendChild(FRAGMENT);
-
-USER_SIMILAR.classList.remove(`hidden`);
